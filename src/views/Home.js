a5a6cc083c69e52e6867fd5142f636434b6cbb62
Pamelajharely83 <pamelajharely83@gmail.com>
2024-08-02 12:14:56 -0500

    Creacion de archivo router.js

---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+const view_home = document.createElement('div');
+view_home.textContent = 'Hi hi';
+view_home.appendChild('#root')
+/* console.log(view_home) */
+
+return view_home;
+};
+
+export default Home;
